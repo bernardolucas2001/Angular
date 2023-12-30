83448708037c0026506cda774d71efd1beb335bd
Bernardo Lucas de Sousa <bernardosaopaulino@gmail.com>
2023-12-30 13:20:34 -0300

    Finalização

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,14 @@ import { RouterOutlet } from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { HeaderComponent } from './views/header/header.component';
 import { HomeComponent1 } from './views/home/home.component';
+import { FooterComponent } from './views/footer/footer.component';
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatToolbarModule, HeaderComponent, HomeComponent1],
+  imports: [CommonModule, RouterOutlet, MatToolbarModule, HeaderComponent, HomeComponent1,FooterComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
